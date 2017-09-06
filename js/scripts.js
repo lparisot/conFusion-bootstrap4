2d90b36f21b09c729f1120e14ed009a1db2a3628
Laurent Parisot <parisot.laurent@gmail.com>
2017-09-06 14:42:33 +0200

    watch change in scss files and reload

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("#carousel-button").click(function() {
+    $("#mycarousel").carousel( {interval: 2000} );
+    if ($("#carousel-button").children("span").hasClass('fa-pause')) {
+      $("#mycarousel").carousel('pause');
+      $("#carousel-button").children("span").removeClass('fa-pause');
+      $("#carousel-button").children("span").addClass('fa-play');
+    }
+    else if ($("#carousel-button").children("span").hasClass('fa-play')) {
+      $("#mycarousel").carousel('cycle');
+      $("#carousel-button").children("span").removeClass('fa-play');
+      $("#carousel-button").children("span").addClass('fa-pause');
+    }
+  });
+});
